9cf3afebab17e06727536c56fe0447c2cacc3632
amal918b <amal918b@stud.kea.dk>
2018-05-15 10:30:45 +0200

    add day 1

---
+++ day1/main-secure.js
@@ -0,0 +1,58 @@
+"use strict"; //we can split names and return something as URLs could be a case too, pretty cool, functions to analyze a function and return something to manipulate
+//    "firstName middleName lastName"
+
+// fullName = "Amalia Blanca Plana"
+// firstName = str(indexOf)
+// middleName = substr(firstName, indexOf)
+// lastName = substr(middleName, indexOf)
+
+const fullName = "Amalia Blanca Plana";
+
+//day 2
+/* function writeFullName(firstName, lastName){
+    const fullName = `fullname ${firstName} ${lastName}`;
+ //   const fullName = firstName + middleName + lastName;
+    console.log(fullName);
+} */
+function writeName(fullName){
+
+//call split name
+const names = splitName (fullName);
+// wrong try....return splitName();
+
+// console log all name words
+  console.log(`firstname: ${firstName}`);
+  console.log(`lastname: ${middleName}`);
+  console.log(`middlename: ${lastName}`);
+}
+function splitName(fullName) {
+  const firstSpace = fullName.indexOf(" ");
+  const secondSpace = fullName.indexOf(" ", firstSpace + 1);
+
+  const firstName = capitalize(fullName.substr(0, firstSpace));
+  const middleName = capitalize(fullName.substring(firstSpace+1, secondSpace));
+  const lastName = capitalize(fullName.substr(secondSpace+1));
+
+//   console.log(`firstname: ${firstName}`);
+//   console.log(`lastname: ${middleName}`);
+//   console.log(`middlename: ${lastName}`);
+
+const result = { //will return an object in console, will generate keynames but will order by alhabetic 
+    firstName,
+    middleName,
+    lastName
+}
+
+}
+function capitalize(str) {
+  const firstLetter = str.trim().substr(0, 1).toUpperCase(); //trim is to take out spaces not needed, might mess up with string in JS
+  const theRest = str.trim().substr(1).toLowerCase();
+
+  const result = firstLetter + theRest;
+
+  return result; //to test on console put splitName("amalia blanca plana")
+}
+
+// const filename = "image.jpg";
+// const lastDot = filename.lastIndexOf(".");
+// const filename = filename.substring( lastDot+1 )
---
+++ day1/main.js
@@ -0,0 +1,34 @@
+"use strict" //warn of double called functions and stuff
+
+const name = "Peter";
+let email; //this would make an error if it was a const
+            //if i put email on console would return undefined
+ let email = null; //in console will return null, better than set them to undefined           
+let hello = true; //i know that can only be true/false
+
+//concatenation
+const firstName = "Peter";
+const lastName = "Hi";
+
+const fullName = firstName + lastName;
+
+//concatenating the following will be a number, therefore is a calculation
+const n1 = 1;
+const n2 = 2;
+//concatenating the following will be a string
+const s1 = "1";
+const s2 = "2";
+//concatenating numbers and strings will return strings but can force into number by Number(s1)
+//NaN is Not a Number, should have been but it isn't
+
+const greeting = "Hello "+firstName+" how are you doing?"
+console.log(greeting);
+
+let aname = 
+
+const greeting3 = "Hello ${firstName},
+                how are you doing
+                    Guess fine";
+
+// exercise for 3 name words "Amalia Blanca Plana"
+
